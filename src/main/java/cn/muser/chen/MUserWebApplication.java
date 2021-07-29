366fff79ab3b1a352c308ef30536d4bb77a975c8
geekerchen <hkbarryc@gmail.com>
2021-07-29 17:41:33 +0800

    muser

---
+++ src/main/java/cn/muser/chen/MUserWebApplication.java
@@ -0,0 +1,13 @@
+package cn.muser.chen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MUserWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MUserWebApplication.class, args);
+    }
+
+}
